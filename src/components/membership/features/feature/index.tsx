3687476b02bfb65d429b2e2ffed5855096d647b4
husanbek123 <husanbek555@gmail.com>
2023-07-25 18:22:16 +0500

    Membership v2

--- src/components/membership/features/feature/index.tsx
+++ src/components/membership/features/feature/index.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./index.module.scss";
+import Image from "next/image";
 
 type Props = {
   title: string;
   description: string;
+  imgSrc: string;
 } & React.HTMLProps<HTMLDivElement>;
 
-
-
-
-export default function Feature({ title, description }: Props) {
+export default function Feature({ title, description, imgSrc }: Props) {
   return (
     <div className={styles.feature}>
       <h2>{title}</h2>
+      <div>
+        <img src={imgSrc} alt="" width={300} height={200} />
+      </div>
       <p>{description}</p>
     </div>
   );
